84ae495bb6ff0129968b1bac161a6a29ebe63032
apophis51 <malcolmxvernon@hotmail.com>
2022-12-06 14:14:53 -0500

    lesson 1

---
+++ unit_1/# lesson.py
@@ -0,0 +1,64 @@
+# Weclcome to PDX CODE Guild!
+# single-line comments use pound sign #
+# everything to the right of the pound sign will be a comments
+""""
+Multi-line
+comment with
+double quote
+"""
+#.........................................................................#
+
+# a "function" is a named piece of code that performs a specific task
+# Python comes with many built-in functions
+# functions must be executed using parentheses after their name ()
+# 
+# print(data) - a built-in function to display the 'data' in the terminal
+
+# express an opinion about spam
+#print("I don't like spame!")
+
+
+# Datatypes: string (str)
+
+# stings are sequences of textual characters surounded by quotes
+
+#'cats'
+#"dog"
+#'01239019458031'   # floats
+#"3.23409328023"    #integers
+#''   # blank string
+#"" # blank string
+#.upper()
+#.title()
+#\n newline character   \t - horizontal tab character
+
+# \ are put before the character and would normally cause error
+print("f".upper())
+
+print("whats up  .lower()")
+
+print('A\\nB\tf\nC')
+
+#1.1 Each letter from the word "python" in its own string
+print("P"+"y"+"t"+"h"+"o"+"n")
+
+#1.2 Each individual word is printed
+print("Python"+' '+"is"+' '+"Fun")
+
+#1.3 The individual characters in a sentences including the spaces and punctuation in its own string
+print("P"+"y"+"t"+"h"+"o"+"n"+" "+"i"+"s"+" "+"f"+"u"+"n"+"!")
+
+
+print("pythonn"+"python")
+
+print("\n\n\n\n\n")
+
+
+
+print("*'-.-'*'-.-'*'-.-'*'--'*Project: PythonVersion: 1.0Author: Guido van Rossum Email: bdfl@python.com Date: Jan, 1994 *'-.-'*'-.-'*'-.-'*'--'* ")
+
+print("\n\n\n*'-.-'*'-.-'*'-.-'*'--'* \n\tProject: Python \n\t\t Version: 1.0  \n\tAuthor: Guido van Rossum \n\tEmail: bdfl@python.com \n\tDate: Jan, 1994 \n\t*'-.-'*'-.-'*'-.-'*'--'* ")
+
+
+
+print("fjshf fjskjf sfkjskf".capitalize())
